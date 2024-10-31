08a1559629985e47c55ff6e408c8f045cae07c3b
flabiohs <flabiohs@gmail.com>
2024-10-31 10:26:21 -0500

    fix:configuration rutar

--- user/MsvcUser.go
+++ user/MsvcUser.go
@@ -10,6 +10,7 @@ import (
 
 	"net/http"
 
+	constants "github.com/flabio/safe_constants"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/safe/utils"
@@ -18,36 +19,35 @@ import (
 // Handler for the  service
 func MsvcUser(namePath string, c *fiber.Ctx) error {
 	err := godotenv.Load(".env")
-
 	if err != nil {
 		log.Fatalf(utils.ENV_ERROR)
 	}
 	url := os.Getenv("MSVC_USER_URL")
 	pageParam := c.Query(utils.PAGE)
-
 	id := c.Params(utils.ID)
-
 	if len(id) != 0 && url != "" {
 		url = url + "/" + id
 	} else {
-		url = url + "?page=" + pageParam
+		url = os.Getenv("MSVC_USER_URL") + "?page=" + pageParam
 	}
+
 	if namePath != "" {
-		url = url + "/" + namePath + "/" + "?page=" + pageParam
+		url = os.Getenv("MSVC_USER_URL") + "/" + namePath + "/" + "?page=" + pageParam
 	}
-	// Crear un nuevo buffer para almacenar el multipart form
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
-	// Procesar los campos del formulario
-	formFields := []string{"first_name", "first_sur_name", "secon_sur_name", "address",
-		"phone",
-		"zip_code",
-		"state_id",
-		"rol_id",
-		"email",
-		"password",
+	formFields := []string{
+		constants.FIRST_NAME,
+		constants.LAST_NAME,
+		constants.ADDRESS,
+		constants.PHONE,
+		constants.STATE_ID,
+		constants.ROL_ID,
+		constants.EMAIL,
+		constants.PASSWORD,
 		"password_confirmation",
-		"active"}
+		constants.ACTIVE,
+	}
 	for _, field := range formFields {
 		value := c.FormValue(field)
 		log.Println(value)
@@ -58,54 +58,95 @@ func MsvcUser(namePath string, c *fiber.Ctx) error {
 			}
 		}
 	}
-
-	// Procesar el archivo subido
 	fileHeader, err := c.FormFile(utils.FILE)
-
 	if err == nil {
 		file, err := fileHeader.Open()
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_OPEN)
 		}
 		defer file.Close()
-
 		part, err := writer.CreateFormFile(utils.FILE, fileHeader.Filename)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_CREATE)
 		}
-
 		_, err = io.Copy(part, file)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_COPY_ERROR)
 		}
 	}
-	// Cerrar el writer
 	err = writer.Close()
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_WRITER_ERROR)
 	}
-
-	// Crear la solicitud al servicio externo
 	req, err := http.NewRequest(c.Method(), url, body)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_REQUEST)
 	}
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 	req.Header.Set(utils.AUTHORIZATION, c.Get(utils.AUTHORIZATION))
-
-	// Enviar la solicitud
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return c.Status(fiber.StatusServiceUnavailable).SendString(utils.FILE_ERROR_SERVICE)
 	}
 	defer resp.Body.Close()
-
-	// Leer la respuesta
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_REQUEST_SERVICE)
 	}
+	return c.Send(respBody)
+}
 
+func MsvcUserAvatar(namePath string, c *fiber.Ctx) error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf(utils.ENV_ERROR)
+	}
+	url := os.Getenv("MSVC_USER_URL")
+	id := c.Params(utils.ID)
+
+	if len(id) != 0 && namePath != "" {
+		url = url + "/" + namePath + "/" + id
+		log.Println(url)
+	}
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	fileHeader, err := c.FormFile(utils.FILE)
+	if err == nil {
+		file, err := fileHeader.Open()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_OPEN)
+		}
+		defer file.Close()
+		part, err := writer.CreateFormFile(utils.FILE, fileHeader.Filename)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_CREATE)
+		}
+		_, err = io.Copy(part, file)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_COPY_ERROR)
+		}
+	}
+	err = writer.Close()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_WRITER_ERROR)
+	}
+	req, err := http.NewRequest(c.Method(), url, body)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_REQUEST)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set(utils.AUTHORIZATION, c.Get(utils.AUTHORIZATION))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return c.Status(fiber.StatusServiceUnavailable).SendString(utils.FILE_ERROR_SERVICE)
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(utils.FILE_ERROR_REQUEST_SERVICE)
+	}
 	return c.Send(respBody)
 }
